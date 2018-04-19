ce3c6affb5c107772115a435eb48194edb49eed2
Walter Simoncini <walterdev@icloud.com>
2018-04-19 18:22:16 +0200

    Implemented email search function

--- icloud_cleaner.py
+++ icloud_cleaner.py
@@ -2,8 +2,20 @@ import imaplib
 from configobj import ConfigObj
 
 def connect(config):
-    mail = imaplib.IMAP4_SSL(config["imap_server"], config["imap_port"])
-    mail.login(config["username"], config["password"])
+    email_connection = imaplib.IMAP4_SSL(config["imap_server"], config["imap_port"])
+    email_connection.login(config["username"], config["password"])
+    email_connection.select("inbox")
+
+    return email_connection
+
+def search_emails(email_connection, sender):
+    typ, data = email_connection.search(None, '(FROM "' + sender + '")')
+    
+    # Split the email identifiers in an array
+    mail_ids = data[0]
+    return mail_ids.split()
+
 
 config = ConfigObj("config.ini")
-imap_conn = connect(config)
+email_connection = connect(config)
+search_emails(email_connection, "example@gmail.com")
