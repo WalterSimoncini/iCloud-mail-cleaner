1f70a707361e5cbf0f5e222bcd9d127d18888881
Walter Simoncini <walterdev@icloud.com>
2018-04-19 20:05:46 +0200

    Updated the program to use the email UID in accordance with the IMAP specifications. Implemented the deletion of the target emails

--- icloud_cleaner.py
+++ icloud_cleaner.py
@@ -6,9 +6,9 @@ import email.header
 from configobj import ConfigObj
 
 def connect(config):
-    email_connection = imaplib.IMAP4_SSL(config["imap_server"], config["imap_port"])
-    email_connection.login(config["username"], config["password"])
-    email_connection.select("inbox")
+    email_connection = imaplib.IMAP4_SSL(config['imap_server'], config['imap_port'])
+    email_connection.login(config['username'], config['password'])
+    email_connection.select('inbox')
 
     return email_connection
 
@@ -20,22 +20,29 @@ def search_emails(email_connection, sender):
     return mail_ids.split()
 
 # Add the deleted flag to an email
-def set_deleted(email_connection, email_id):
-    email_connection.store(email_id, '+FLAGS', '\\Deleted')
+def set_deleted(email_connection, email_uid):
+    email_connection.uid("STORE", int(email_uid), "+FLAGS", "(\\Deleted)")
 
 def fetch_uid(email_connection, email_id):
     status, uid_string = email_connection.fetch(email_id, 'UID')
-    return re.search(r'\((.*?)\)', uid_string[0]).group(1).replace("UID ", "")
+    return re.search(r'\((.*?)\)', uid_string[0]).group(1).replace('UID', '')
 
-config = ConfigObj("config.ini")
+config = ConfigObj('config.ini')
 email_connection = connect(config)
 
-emails = search_emails(email_connection, "example@gmail.com")
-print("Fetched " + str(len(emails)) + " emails for " + "address")
+emails = search_emails(email_connection, 'mail@example.com')
+print('Fetched ' + str(len(emails)) + ' emails for ' + 'address')
 
 for e in emails:
+    # The fetching of the email UID is required
+    # since the email ID may change between operations
+    # as specified by the IMAP standard
     uid = fetch_uid(email_connection, e)
-    # set_deleted(email_connection, e)
+    set_deleted(email_connection, uid)
 
 # Confirm the deletion of the messages
-# email_connection.expunge()
+email_connection.expunge()
+
+# Close the mailbox and logout
+email_connection.close()
+email_connection.logout()
